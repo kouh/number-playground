0dc3e0a5de37027e7aa848ea6d8b199e40ae81fe
kouh <kouh28@gmail.com>
2014-10-17 22:35:24 +0900

    Add number table.

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('hello world');
+
+
+$(function(){
+  var $table = $('#tbl');
+  var row = 10;
+  var col = 10;
+  var start = 0;
+
+  for (var i = 0; i<row; i++) {
+    var $tr = $('<tr>');
+    for(var j=0; j<col; j++){
+      $tr.append($('<td>')
+        .text(i*col+j+start)
+        .css({
+          width: "3em",
+          height: "3em"
+        })
+        .on('click',function(){
+          $(this).toggleClass('hide');
+        })
+      );
+    }
+    $table.append($tr);
+  };
+
+  
+  
+});
